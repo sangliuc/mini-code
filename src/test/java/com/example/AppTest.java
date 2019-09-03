337fbfe8dbe3e6977bf950767bdc3b914f0ad8d6
olisang <sangliuc@163.com>
2019-09-03 19:54:52 +0800

    第一次提交

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testLetterCombinations() {
+        System.out.println(new App().letterCombinations("2"));
+    }
+}
